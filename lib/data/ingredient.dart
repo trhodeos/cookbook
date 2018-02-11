af1e3e4eedec1637a05e14685c4df1336b8a77bc
Tyler Rhodes <tyler.s.rhodes@gmail.com>
2018-02-11 12:38:28 -0800

    Add a few mock recipes.

--- lib/data/ingredient.dart
+++ lib/data/ingredient.dart
@@ -3,6 +3,7 @@ enum AmountType {
   liquid,
   count,
   weight,
+  measurement,
 }
 enum LiquidUnit {
   unset,
@@ -10,24 +11,36 @@ enum LiquidUnit {
 enum WeightUnit {
   unset,
 }
+enum MeasurementUnit {
+  unset,
+  ounce,
+  teaspoon,
+  tablespoon,
+  cup,
+}
 
 class Amount {
-  final double amount;
+  final num amount;
   final LiquidUnit liquidUnit;
   final WeightUnit weightUnit;
+  final MeasurementUnit measurementUnit;
   final AmountType type;
-  Amount._internal(this.amount, this.type, {this.liquidUnit: LiquidUnit.unset, this.weightUnit: WeightUnit.unset});
+  Amount._internal(this.amount, this.type, {this.liquidUnit: LiquidUnit.unset, this.weightUnit: WeightUnit.unset, this.measurementUnit: MeasurementUnit.unset});
 
-  factory Amount.liquid(double amount, LiquidUnit liquidUnit) {
+  factory Amount.liquid(num amount, LiquidUnit liquidUnit) {
     return new Amount._internal(amount, AmountType.liquid, liquidUnit: liquidUnit);
   }
 
-  factory Amount.count(double amount) {
+  factory Amount.count(num amount) {
     return new Amount._internal(amount, AmountType.count);
   }
 
-  factory Amount.weight(double amount, WeightUnit weightUnit) {
+  factory Amount.weight(num amount, WeightUnit weightUnit) {
     return new Amount._internal(amount, AmountType.weight, weightUnit: weightUnit);
+  }
+
+  factory Amount.measurement(num amount, MeasurementUnit measurementUnit) {
+    return new Amount._internal(amount, AmountType.measurement, measurementUnit: measurementUnit);
   }
 }
 
